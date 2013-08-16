727925912196c34222c5edef41d3ce8f628217ce
oberon <st900278@gmail.com>
2013-08-16 08:57:49 +0800

    Finish fetching Taipei Bus

--- Taipei-Bus/Web-Fetch.py
+++ Taipei-Bus/Web-Fetch.py
@@ -5,10 +5,11 @@ from PyQt4 import QtCore
 from bs4 import BeautifulSoup
 import urllib
 import urllib2
+import json
 "Fetch Catalog Web"
 
 url = "http://pda.5284.com.tw/MQS/businfo1.jsp"
-bus_info = []
+bus_info = {}
 index = 0
 "Fetch Bus List"
 ghost = Ghost()
@@ -21,28 +22,22 @@ data = [unicode(x).encode("utf8") for x in result]
 for bus in data:
 	bus_url = "http://pda.5284.com.tw/MQS/businfo2.jsp?routename=" + bus
 	print bus_url
-	bus_info.append(bus)
+	bus_info[bus] = {"name":bus,"forward":[],"backward":[]}
 	
 	req = urllib2.Request(bus_url)
 	f = urllib2.urlopen(req)
 	bus_index = f.read()
 	soup = BeautifulSoup(bus_index)
 	table = soup.find_all("table")
+
 	for bus_stop in table[2].find_all("tr"):
 		td_data = bus_stop.find_all("td")
-		print td_data[0].string
-		print td_data[0].a['href']
-		if(td_data[1].find("font") != None):
-			td_data[1].font.extract()
-		print td_data[1].string
+		bus_info[bus]["forward"].append((td_data[0].string,td_data[0].a['href']))
 	for bus_stop in table[3].find_all("tr"):
 		td_data = bus_stop.find_all("td")
-		print td_data[0].string
-		print td_data[0].a['href']
-		if(td_data[1].find("font") != None):
-			td_data[1].font.extract()
-		print td_data[1].string
-
+		bus_info[bus]["backward"].append((td_data[0].string,td_data[0].a['href']))
+	
+print json.dumps(bus_info,sort_keys=True)
 
 
 
